9faab81f0b7571293b6f9b6acc2a6c6cbcd19cb7
King-hamster632014 <bwatson@taipa.school.nz>
2026-05-25 12:50:09 +1200

    Mod_2

---
+++ Super_sand.js
@@ -0,0 +1,10 @@
+// my_first_mod.js
+
+elements.super_sand = {
+    color: "#a020f0", // Hex color code for purple
+    behavior: behaviors.POWDER, // How it acts (like sand)
+    category: "land", // Which menu tab it appears in
+    state: "solid", // Solid, liquid, or gas
+    density: 1600, // How heavy it is
+    desc: "?" // Description when you hover over it
+};
